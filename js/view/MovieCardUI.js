473f67d968e0d90d7f64761f43f56ac169a291dc
Klement Baastrup Johansen <kljo@campusvejle.dk>
2018-06-14 16:27:47 +0200

    _W_ main

--- js/view/MovieCardUI.js
+++ js/view/MovieCardUI.js
@@ -29,15 +29,13 @@ class MovieCardUI{
     //     this.template = value;
     // }
 
-    static createMovieCardUI(){
-        
-        // Opret testdata.
-        Movie.createTestData();
-        
+    static createMovieCard(){
+
         // createMovieCard for hver instans i Movie.Instances. 
         let movieInstanceKeys = Object.keys(Movie.instances);
         
-        
+        console.log('test')
+
         // Hvordan får du returneret dine instanser keys i et array?
         for (let index = 0; index < movieInstanceKeys.length; index++) {
             const key = movieInstanceKeys[index];
@@ -47,10 +45,15 @@ class MovieCardUI{
             let movieElement = document.getElementById('movieSection');
             
 
+            // let card = '<article class="test">' + Movie.instances[key].title + '</article>';
+            let card = document.createElement('article').classList.add('col s4')
+            console.log(card)
+            console.log('test')
 
-            let card = '<article class="test">' + Movie.instances[key].title + '</article>';
 
-            movieElement.innerHTML = card;
+            // movieElement.innerHTML = card;
+
+            // card++
 
 
             // let productClone = document.importNode(MovieCardUI.template.content, false);
@@ -77,7 +80,20 @@ class MovieCardUI{
         // 
     }
 
+    // Hvis der kommer et nyt Movie Card efter UI er sat op.
     static addCard(){
-        
+
+    }
+
+    static setupUserInterface(){
+        // Opret testdata.
+        Movie.createTestData();
+
+        // Create Movie cards
+        this.createMovieCard();
+
+        // Populate movieSection in DOM
+        let movieSection = document.getElementById('movieSection')
+        // movieSection.appendChild({'test': 'test'})
     }
 }
