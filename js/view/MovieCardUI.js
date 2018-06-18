94ff556b967770a75f2234ad4bc2d363fada3969
Klement Baastrup Johansen <kljo@campusvejle.dk>
2018-06-18 19:53:51 +0200

    small fixes

--- js/view/MovieCardUI.js
+++ js/view/MovieCardUI.js
@@ -1,6 +1,6 @@
 
 // Load from localStorage. If empty createTestData() is run creating Movie.instances and saving to localStorage['movieDB'];
-Movie.loadAll();
+// Movie.loadAll();
 
 class MovieCard {
     
@@ -20,38 +20,38 @@ class MovieCard {
     
     render(){
         return  `<article class="col s12 m6 l4 xl3">
-        <div class="card medium hoverable">
-        <div class="card-image">
-        <img src="./img/${this.img}">
-        </div>
-        <div class="card-content black white-text">
-        <span class="card-title activator">${this.title}<i class="material-icons right">&#8593;</i></span>
-        <p class="card-year">Year: ${this.year}</p>
-        <p class="card-genre">Genre: ${this.genre}</p>
-        <p><a id="favBtn" href="#" onclick="clickHandler()" data-key="${this.key}">Add to Favorites</a></p>
-        </div>
-        <div class="card-reveal black white-text">
-        <span class="card-title white-text ">${this.title}<i class="material-icons right">x</i></span>
-        <p class="card-description">${this.description}</p>                            
-        </div>
-        </div>
-        </article>`;
+                    <div class="card medium hoverable">
+                        <div class="card-image">
+                            <img src="./img/${this.img}">
+                        </div>
+                    <div class="card-content black white-text">
+                        <span class="card-title activator">${this.title}<i class="material-icons right">&#8593;</i></span>
+                        <p class="card-year">Year: ${this.year}</p>
+                        <p class="card-genre">Genre: ${this.genre}</p>
+                        <p><a id="favBtn" href="#" onclick="clickHandler()" data-key="${this.key}">Add to Favorites</a></p>
+                    </div>
+                    <div class="card-reveal black white-text">
+                        <span class="card-title white-text ">${this.title}<i class="material-icons right">x</i></span>
+                        <p class="card-description">${this.description}</p>                            
+                        </div>
+                    </div>
+                </article>`;
     }
     
-    static createMovieCards(){
+    // static createMovieCards(){
         
-    }
+    // }
     
     // Change MovieCard.favorite to true;
     // ????
-    static clickHandler(){
-        console.log('link clicked')
-    }
+    // static clickHandler(){
+    //     console.log('link clicked')
+    // }
 
     static changeState(){
         let movie = Movie.instances[key];
 
-        document.querySelector("[data-key=" + movie + "]").innerHTML = 'Remove';
+        document.querySelector(`[data-key="${movie}"]`).innerHTML = 'Remove';
         // this.favorite = true;
         // let favoriteMovies = [];
         return movie.favorite;
@@ -61,9 +61,9 @@ class MovieCard {
     static setupUserInterface(){
         
         let movieSection = document.getElementById('movieSection'),
-        favoriteSection = document.getElementById('favoriteSection'),
-        keys = [],
-        movies = {};
+            favoriteSection = document.getElementById('favoriteSection'),
+            keys = [],
+            movies = {};
         
         Movie.loadAll();
         
@@ -86,4 +86,4 @@ class MovieCard {
     
 }
 
-MovieCard.setupUserInterface();
+// MovieCard.setupUserInterface();
