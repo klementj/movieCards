8991535345ad2776bb474b278782f391682c9d99
Klement Baastrup Johansen <kljo@campusvejle.dk>
2018-12-09 12:06:02 +0100

    Importing babel polyfill and materialize js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
+// Temp fix for 'regeneratorRuntime is not defined' webpack error
+import 'babel-polyfill'
+
+// Import 
 import './scss/main.scss'
+
 // Import JS files
-import 'materialize-css'
+import 'materialize-css/dist/js/materialize'
 import './js/initialize'
+
+
+
+// import './js/view/MovieCard'
+// window.customElements.define('movie-card', MovieCard)
+
